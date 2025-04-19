8b4357bffe2e1f2828131764098f4ac3d92be06a
psrtech <karthickprabakaran06@gmail.com>
2025-04-19 21:16:17 +0530

    Practiced the imports and exports

---
+++ REACT/JS refresher/00-starting-js-proj/assets/scripts/api.js
@@ -0,0 +1,5 @@
+
+
+let key = "Hello";
+
+export default key;
---
+++ REACT/JS refresher/00-starting-js-proj/assets/scripts/script.js
@@ -0,0 +1,12 @@
+import key from './api.js';
+
+//console.log(key);
+
+
+const user = 
+{
+    name : "Max",
+    age : 20
+}
+
+console.log(Max);
