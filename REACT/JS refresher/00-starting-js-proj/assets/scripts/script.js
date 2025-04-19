c0b681932c81fc1e4f378decf152507ee4979f14
psrtech <karthickprabakaran06@gmail.com>
2025-04-19 21:28:50 +0530

    Practiced the Class

--- REACT/JS refresher/00-starting-js-proj/assets/scripts/script.js
+++ REACT/JS refresher/00-starting-js-proj/assets/scripts/script.js
@@ -3,10 +3,26 @@ import key from './api.js';
 //console.log(key);
 
 
-const user = 
-{
-    name : "Max",
-    age : 20
-}
+// const user = 
+// {
+//     name : "Max",
+//     age : 20
+// }
 
-console.log(Max);
+// console.log(Max);
+
+
+// class User{
+
+//     constructor (name,age){
+//         this.name = name;
+//         this.age = age
+//     }
+// }
+
+
+// let user1 = new User("max" , 31);
+
+// console.log(user1.age);
+
+
