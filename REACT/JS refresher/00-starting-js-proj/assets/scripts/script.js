8076e001daf50c85bd04c970fcf6d22c806958d0
psrtech <karthickprabakaran06@gmail.com>
2025-04-20 19:59:41 +0530

    Practiced the Destructuring and Spread operator

--- REACT/JS refresher/00-starting-js-proj/assets/scripts/script.js
+++ REACT/JS refresher/00-starting-js-proj/assets/scripts/script.js
@@ -26,6 +26,37 @@ import key from './api.js';
 // console.log(user1.age);
 
 
-function transformToObjects(numberArray) {
-    return numberArray.map(item => ({ val: item }));
-}
+// function transformToObjects(numberArray) {
+//     return numberArray.map(item => ({ val: item }));
+// }
+
+
+// const [firstName, lastName] = ["max", "raghul"];
+
+// console.log(firstName);
+
+
+// spread operator
+
+
+// let firstArr = ["max", "ram"];
+
+// let secondArr = ["sita", "well", ...firstArr];
+
+// console.log(secondArr);
+
+
+// let obj = {
+//     name: "ram",
+//     age: 21
+// }
+
+// let newObj = {
+//     phone: 1231231,
+//     ...obj
+// }
+
+// console.log(newObj);
+
+
+
